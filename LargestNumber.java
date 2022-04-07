752944079a12d39a21cb293bb3511d973ec51cc3
Rakshitmv <rakshit8976646711@gmail.com>
2022-04-07 19:34:38 +0530

    [Rakshit] Modify: User Input largest number among 3 number

--- LargestNumber.java
+++ LargestNumber.java
@@ -1,13 +1,22 @@
+import java.util.Scanner;
+
 public class LargestNumber {
     public static void main(String[] args) {
-        int a = 40, b = 20, c = 30;
-        if (a >= b && a >= c) {
-            System.out.println(" a is the largest Number with " + a);
-        } else if (b >= a && b >= c) {
-            System.out.println(" b is the largest Number with " + b);
-        } else {
-            System.out.println(" c is the largest number with " + c);
-        }
+        int a, b, c, largest, temp;
 
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the first number:");
+        a = sc.nextInt();
+        System.out.println("Enter the second number:");
+        b = sc.nextInt();
+        System.out.println("Enter the third number:");
+        c = sc.nextInt();
+
+        temp = a > b ? a : b;
+
+        largest = c > temp ? c : temp;
+
+        System.out.println("The largest number is: " + largest);
     }
 }
